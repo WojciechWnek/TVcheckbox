a4bab46d3e6937dfb99778ee1e8266a9804e7874
unknown <wojciech.wnek97@gmail.com>
2022-01-15 19:52:38 +0100

    Finished content layout

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,3 +2,4 @@ export const API_KEY = "api_key=6b51cc23bca4d4e7532b1fc9c8c8f576";
 export const API_URL = "https://api.themoviedb.org/3/";
 export const API_IMAGE_PATH = "https://image.tmdb.org/t/p/original";
 export const RES_PER_PAGE = 5;
+export const UNIX_EPOCH = 1970;
--- assets/js/showContentView.js
+++ assets/js/showContentView.js
@@ -1,4 +1,4 @@
-import { API_IMAGE_PATH } from "./config.js";
+import { API_IMAGE_PATH, UNIX_EPOCH } from "./config.js";
 import View from "./View.js";
 
 class ShowContentView extends View {
@@ -22,12 +22,17 @@ class ShowContentView extends View {
     }
 
     _generateMarkup() {
-        console.log(this);
         if (this._data.media_type === "movie") {
             return `
-        <img class="content__poster" src="${API_IMAGE_PATH}${
-                this._data.poster_path
-            }" alt="${this._data.title}">
+        <img class="content__poster" src="${
+            this._data.poster_path
+                ? API_IMAGE_PATH + this._data.poster_path
+                : "assets/img/blank_poster.svg"
+        }" alt="${
+                this._data.title
+                    ? "Poster of " + this._data.title
+                    : "Movie poster"
+            }">
 
         <div class="content__note">
             <h1 class="content__title">${
@@ -69,9 +74,13 @@ class ShowContentView extends View {
             </button>
         </div>
             
-        <p class="content__overview">
-            ${this._data.overview ? this._data.overview : "No info"}
-        </p>
+        <div class="content__overview">
+            ${
+                this._data.overview
+                    ? this._showOverview(this._data.overview)
+                    : "No info about this movie"
+            }
+        </div>
         <ul class="content__genres">
             ${this._showGenres()}
         </ul>
@@ -80,72 +89,138 @@ class ShowContentView extends View {
 
         if (this._data.media_type === "tv") {
             return `
-        <img class="content__poster" src="${API_IMAGE_PATH}${
+            <img class="content__poster" src="${
                 this._data.poster_path
-            }" alt="${this._data.name}">
-
-        <div class="content__note">
-            <h1 class="content__title">${
+                    ? API_IMAGE_PATH + this._data.poster_path
+                    : "assets/img/blank_poster.svg"
+            }" alt="${
                 this._data.name
-                    ? this._data.name
-                    : this._data.original_name
-                    ? this._data.original_name
-                    : ""
-            }
-            </h1>
-            <p class="content__status">${
-                this._data.in_production === true ? "Ongoing" : "Ended"
-            }</p>
-            <p class="content__lastAir">${
-                this._data.last_air_date
-                    ? "Last air date: " + this._data.last_air_date
-                    : ""
-            }</p>
+                    ? "Poster of " + this._data.name
+                    : "Series poster"
+            }">
 
-        </div>
+            <div class="content__note">
+                <h1 class="content__title">${
+                    this._data.name
+                        ? this._data.name
+                        : this._data.original_name
+                        ? this._data.original_name
+                        : ""
+                }
+                </h1>
+                <p class="content__status">${
+                    this._data.status ? "Status: " + this._data.status : ""
+                }</p>
+                <p class="content__lastAir">${
+                    this._data.last_air_date
+                        ? "Last air date: " + this._data.last_air_date
+                        : ""
+                }</p>
 
-        <div class="content__bookmarks">
+            </div>
 
-            <button class="btn btn--bookmark" data-bookmark="toWatch">
+            <div class="content__bookmarks">
+
+                <button class="btn btn--bookmark" data-bookmark="toWatch">
+                    <img class="icon" src="assets/img/${
+                        this._data.bookmarked === "toWatch"
+                            ? this._data.bookmarked + "--marked"
+                            : "toWatch"
+                    }.svg" alt="bookmark">
+                </button>
+                <button class="btn btn--bookmark" data-bookmark="watching">
                 <img class="icon" src="assets/img/${
-                    this._data.bookmarked === "toWatch"
+                    this._data.bookmarked === "watching"
                         ? this._data.bookmarked + "--marked"
-                        : "toWatch"
+                        : "watching"
                 }.svg" alt="bookmark">
-            </button>
-            <button class="btn btn--bookmark" data-bookmark="watching">
-            <img class="icon" src="assets/img/${
-                this._data.bookmarked === "watching"
-                    ? this._data.bookmarked + "--marked"
-                    : "watching"
-            }.svg" alt="bookmark">
-            </button>
-            <button class="btn btn--bookmark" data-bookmark="watched">
-            <img class="icon" src="assets/img/${
-                this._data.bookmarked === "watched"
-                    ? this._data.bookmarked + "--marked"
-                    : "watched"
-            }.svg" alt="bookmark">
-            </button>
-        </div>
-            
-        <p class="content__overview">
-            ${this._data.overview ? this._data.overview : "No info"}
-        </p>
-        <ul class="content__genres">
-            ${this._showGenres()}
-        </ul>
-
-        <div class="content__seasons seasons">
-            <h1 class="seasons__heading">Seasons</h1>
-            <ul class="seasons__list">
-                ${this._showSeasons()}
+                </button>
+                <button class="btn btn--bookmark" data-bookmark="watched">
+                <img class="icon" src="assets/img/${
+                    this._data.bookmarked === "watched"
+                        ? this._data.bookmarked + "--marked"
+                        : "watched"
+                }.svg" alt="bookmark">
+                </button>
+            </div>
+                
+            <div class="content__overview">
+                ${
+                    this._data.overview
+                        ? this._showOverview(this._data.overview)
+                        : "No info about this series"
+                }
+            </div>
+            <ul class="content__genres">
+                ${this._showGenres()}
             </ul>
-        </div>
+
+            <div class="content__seasons seasons">
+                <h1 class="seasons__heading">Seasons</h1>
+                <ul class="seasons__list">
+                    ${this._showSeasons()}
+                </ul>
+            </div>
         `;
         }
         if (this._data.media_type === "person") {
-            console.log("person");
+            return `
+            <img class="content__poster" src="${
+                this._data.profile_path
+                    ? API_IMAGE_PATH + this._data.profile_path
+                    : "assets/img/blank_profile.svg"
+            }" alt="${
+                this._data.name
+                    ? "Poster of " + this._data.name
+                    : "Profile picture"
+            }">
+
+            <div class="content__note">
+                <h1 class="content__title">${
+                    this._data.name ? this._data.name : ""
+                }
+                </h1>
+                <p class="content__status">${
+                    this._data.birthday ? "Born: " + this._data.birthday : ""
+                }</p>
+                <p class="content__status">${
+                    this._data.deathday ? "Died: " + this._data.deathday : ""
+                }</p>
+            </div>
+                
+            <div class="content__overview">
+                ${
+                    this._data.biography
+                        ? this._showOverview(this._data.biography)
+                        : "No info about this person"
+                }
+            </div>
+            <div class="content__personalInfo">
+                <h1 class="title">Personal info</h1>
+                <p class="info">Known for<br><span class="value">${
+                    this._data.known_for_department
+                        ? this._data.known_for_department
+                        : ""
+                }</span></p>
+                <p class="info">Gender<br><span class="value">${
+                    this._data.gender
+                        ? this._data.gender === 1
+                            ? "Female"
+                            : "Male"
+                        : "No data"
+                }</span></p>
+                <p class="info">Place of birth<br><span class="value">${
+                    this._data.place_of_birth
+                        ? this._data.place_of_birth
+                        : "No data"
+                }</span></p>
+                <p class="info">Age<br><span class="value">${
+                    this._data.birthday
+                        ? this._showAge() + " yeras old"
+                        : "No data"
+                }</span></p>
+            </div>
+        `;
         }
     }
 
@@ -167,11 +242,17 @@ class ShowContentView extends View {
         this._data.seasons.forEach((season) => {
             seasons += `
             <li class="seasons__list__item season">
-                <img class="season__poster icon" src="${API_IMAGE_PATH}${
-                season.poster_path
-            }" alt="${season.name}">
+                <img class="season__poster icon" src="${
+                    season.poster_path
+                        ? API_IMAGE_PATH + season.poster_path
+                        : "assets/img/blank_poster.svg"
+                }" alt="${
+                season.name ? "Poster of " + season.name : "Season poster"
+            }">
                 <div class="season__note">
-                    <h1 class="season__title">${season.name}</h1>
+                    <h1 class="season__title">${
+                        season.name ? season.name : ""
+                    }</h1>
                     <span class="season__airDate">${
                         season.air_date
                             ? "First air date: " + season.air_date
@@ -203,6 +284,34 @@ class ShowContentView extends View {
         ${Math.floor(time) % 60 === 0 ? "" : "m"}
         </span>
         `;
+    }
+
+    _showAge() {
+        const birthday = new Date(this._data.birthday);
+        let aliveUntil;
+        if (this._data.deathday) {
+            aliveUntil = new Date(this._data.deathday).getTime();
+            console.log("here");
+        } else {
+            aliveUntil = Date.now();
+        }
+
+        const differenceInMs = aliveUntil - birthday.getTime();
+        const ageInMs = new Date(differenceInMs);
+        const age = Math.abs(ageInMs.getUTCFullYear() - UNIX_EPOCH);
+
+        return age;
+    }
+
+    _showOverview(overview) {
+        const paragraph = overview.split("\n");
+        let description = "";
+        paragraph.forEach((paragraph) => {
+            description += `
+                <p class="paragraph">${paragraph}</p>
+                `;
+        });
+        return description;
     }
 }
 
